2fe146459006fcd01f5acdb9a03031bd52178bfd
Shuoyuan Huang <shuang375@wisc.edu>
2020-07-17 14:22:22 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# TEMControlServer
+Server for remote communication with TEM microscope server. 
+
+Use `Server.config` to set up remoting configuration. The server expose the remotable object that is used in [TEMControlTest](https://github.com/SyHuang19/TEMControlTest). Remotable class library is found in `bin/Debug/TEMControlWrapper.dll`.
---
+++ TEMControlServer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Remoting;
+
+namespace TEMControlServer
+{
+    class TEMControlRemoteServer
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Server: Reading the config file..");
+            RemotingConfiguration.Configure(@"..\..\Server.config", false);
+
+            Console.WriteLine("Server ready...");
+            Console.ReadLine();
+        }
+    }
+}
